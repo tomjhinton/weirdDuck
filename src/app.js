8ff04bd670d70353a912427566c408932c5bc7b3
Tom Hinton <tomjhinton@gmail.com>
2021-11-29 20:27:32 +0000

    lol

---
+++ src/app.js
@@ -0,0 +1,126 @@
+import './style.scss'
+import * as THREE from 'three'
+
+import { gsap } from 'gsap'
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+
+const gtlfLoader = new GLTFLoader()
+
+
+// console.log(riverMaterial)
+let sceneGroup
+
+
+gtlfLoader.load(
+  'duck.glb',
+  (gltf) => {
+    console.log(gltf)
+
+    // gltf.scene.scale.set(4.5,4.5,4.5)
+    sceneGroup = gltf.scene
+    sceneGroup.needsUpdate = true
+    sceneGroup.position.y -= 3
+    scene.add(sceneGroup)
+
+
+
+
+
+  }
+)
+
+
+const light = new THREE.AmbientLight( 0x404040 ) // soft white light
+scene.add( light )
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.25 )
+scene.add( directionalLight )
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>{
+
+
+
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2 ))
+
+})
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 10
+camera.position.y = 10
+camera.position.z = 15
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.maxPolarAngle = Math.PI / 2 - 0.1
+//controls.enableZoom = false;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  alpha: true
+})
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor( 0x000000, 0 )
+const mouse = new THREE.Vector2()
+
+
+
+
+const clock = new THREE.Clock()
+
+const tick = () =>{
+
+  const elapsedTime = clock.getElapsedTime()
+
+
+  // Update controls
+  controls.update()
+
+
+  // riverMaterial.fragmentShader = fragArray[riverSelected]
+
+
+
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
